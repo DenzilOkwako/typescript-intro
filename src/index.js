547b18dab2b978f83a7bb1d25bdbcf489320fae3
DenzilOkwako <denzilwawire418@gmail.com>
2024-03-09 23:52:55 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+function addNumber(a, b) {
+    return (a + b);
+}
+console.log(addNumber(2, 6));
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function addNumber(a:number,b:number):number{
+    return(a+b);
+}
+
+console.log(addNumber(2,6));
